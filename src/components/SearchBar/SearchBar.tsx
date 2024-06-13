62a5df3143c360bd8664cc5ee441392cbcc1f748
Selma Imširović <selma.imsirovic@stu.ibu.edu.ba>
2024-06-13 13:24:55 +0200

    search image removed

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -9,7 +9,6 @@ const SearchBar = ({ onSearch }) => {
     return (
         <>
             <div className='input-wrapper'>
-                <img className='SearchIcon' src='src/assets/img/search.svg'/>
                 <input placeholder='Type to search...' value={input} onChange={(e) => {onSearch(e.target.value); setInput(e.target.value)}}/>
             </div>
         </>

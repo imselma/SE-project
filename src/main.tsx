9a719452ab0e2afa014951d25f23fc76fce9f189
Selma Imširović <selma.imsirovic@stu.ibu.edu.ba>
2024-06-13 13:12:49 +0200

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store'
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -19,10 +19,10 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 )

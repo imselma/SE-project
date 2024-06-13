995243f9691756f0cd638ba8b835d302cad2670c
Selma Imširović <selma.imsirovic@stu.ibu.edu.ba>
2024-06-13 13:50:40 +0200

    css better

--- src/pages/SingleAdvice.tsx
+++ src/pages/SingleAdvice.tsx
@@ -1,18 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import useDeleteAdvice from '../customHooks/useDeleteAdvice'
-import { Advice } from '../utils/types'
-import useAdviceById from '../customHooks/useAdviceById'
-import EditAdviceModal from '../components/EditAdviceModal'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useDeleteAdvice from '../customHooks/useDeleteAdvice';
+import { Advice } from '../utils/types';
+import useAdviceById from '../customHooks/useAdviceById';
+import EditAdviceModal from '../components/EditAdviceModal';
 
 const SingleAdvice = () => {
-
-    const [adviceData, setAdviceData] = useState<Advice | null>(null)  //ovo je na pocetku undefined ili null, kao sto si npr prije stavljala [], sada s obzirom da koristis odredjen type, moras koristiti null
-    const [id, setId] = useState(localStorage.getItem('adviceID'))
-    const navigate = useNavigate()
-    const userID = localStorage.getItem('userID')
+    const [adviceData, setAdviceData] = useState<Advice | null>(null);
+    const [id, setId] = useState(localStorage.getItem('adviceID'));
+    const navigate = useNavigate();
+    const userID = localStorage.getItem('userID');
     const [openModal, setOpenModal] = useState(false);
     const [editHovered, setEditHovered] = useState(false);
     const [deleteHovered, setDeleteHovered] = useState(false);
@@ -24,21 +23,18 @@ const SingleAdvice = () => {
         if (id) {
             getAdviceById.mutate(id, {
                 onSuccess: (data) => {
-                    console.log("Advice data:", data); 
+                    console.log("Advice data:", data);
                     setAdviceData(data);
                 },
                 onError: (error) => {
-                    console.error("Error geting advices:", error);
+                    console.error("Error getting advice:", error);
                 },
-            })
+            });
         }
-
-    }, [id])
-
+    }, [id]);
 
     const handleDelete = () => {
         if (id) {
-
             deleteAdvice.mutate(id, {
                 onSuccess: () => {
                     console.log("Advice deleted successfully!");
@@ -47,61 +43,81 @@ const SingleAdvice = () => {
                 onError: (error) => {
                     console.error("Error deleting advice:", error);
                 },
-            })
+            });
         }
+    };
 
-    }
     return (
         <>
-            <div className='adviceBody'>
+            <div className='adviceBody' style={{ display: 'flex', justifyContent: 'center' }}>
                 {adviceData && (
-                    <>
-                     <div style={{ margin: '20px', backgroundColor: '#F5F5F6', borderRadius: '8px', width: '1490px', height: '550px', display: 'flex'  }}>
-                            <div className='header' style={{ display: 'flex', flexDirection: 'column' }}>
-                                <div className='title' style={{ marginTop: '20px', marginLeft: '70px' }}>
-                                    <h1>{adviceData.name}</h1>
-                                    <div className="card" style={{ width: '470px', marginTop: '50px' }}>
-                                        <div className="list-group list-group-flush" style={{ display: 'flex', flexDirection: 'row', backgroundColor: '#976B7A', padding: '2px', height: '60px' }}>
-                                            <div className='third'>
-                                                <p style={{ marginLeft: '50px', color: 'white' }}>CREATOR:</p>
-                                                <div style={{ display: 'flex' }}>
-                                                    <h6 style={{ marginLeft: '25px', color: 'white', marginTop: '-10px' }}>{adviceData.user.name} </h6>
-                                                    <h6 style={{ marginLeft: '10px', color: 'white', marginTop: '-10px' }}>{adviceData.user.surname} </h6>
-                                                </div>
-                                            </div>
+                    <div style={{ margin: '20px', backgroundColor: '#F5F5F6', borderRadius: '8px', width: '80%', maxWidth: '1200px' }}>
+                        <div className='header' style={{ display: 'flex', flexDirection: 'column' }}>
+                            <div className='title' style={{ marginTop: '20px', marginLeft: '70px' }}>
+                                <h1>{adviceData.name}</h1>
+                                <div className="card" style={{ width: '100%', marginTop: '20px', backgroundColor: '#976B7A', padding: '10px', borderRadius: '8px' }}>
+                                    <div className='third' style={{ display: 'flex', color: 'white' }}>
+                                        <p style={{ marginLeft: '20px' }}>CREATOR:</p>
+                                        <div style={{ display: 'flex', marginLeft: '10px' }}>
+                                            <h6>{adviceData.user.name}</h6>
+                                            <h6 style={{ marginLeft: '10px' }}>{adviceData.user.surname}</h6>
                                         </div>
                                     </div>
                                 </div>
-                                <div className='rightSide' style={{ display: 'flex', flexDirection: 'column', marginLeft: '10px' }}>
-                                    <div className="card" style={{ width: '425px', height: '150px', border: 'none', marginLeft: '60px', marginTop: '30px', display: 'flex', flexDirection: 'column' }}>
-
-                                        <div className="card-body" style={{ backgroundColor: '#976B7A', borderRadius: '10px' }}>
-                                            <p className="card-text" style={{ color: 'white', textAlign: 'justify', padding: '10px' }}>{adviceData.description}</p>
-                                        </div>
-                                    </div>
-                                    {userID === adviceData.user.id ? (
-                                        <div className='buttons' style={{ display: 'flex', gap: '15px', marginTop: '70px', marginBottom: '20px', marginLeft: '60px' }}>
-                                            <button type="button" className="btn" style={{ backgroundColor: deleteHovered ? '#f2f2f2' : 'white', border: '2px solid #976B7A', color: '#976B7A', width: '125px', height: '45px', fontSize: '18px', fontWeight: 'bold' }}
+                            </div>
+                            <div className='rightSide' style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
+                                <div className="card" style={{ width: '100%', marginTop: '20px', backgroundColor: '#976B7A', borderRadius: '8px', padding: '10px' }}>
+                                    <p style={{ color: 'white', textAlign: 'justify', margin: '0px' }}>{adviceData.description}</p>
+                                </div>
+                                {userID === adviceData.user.id && (
+                                    <div className='buttons' style={{ display: 'flex', gap: '15px', marginTop: '20px' }}>
+                                        <button
+                                            type="button"
+                                            className="btn"
+                                            style={{
+                                                backgroundColor: deleteHovered ? '#f2f2f2' : 'white',
+                                                border: '2px solid #976B7A',
+                                                color: '#976B7A',
+                                                width: '125px',
+                                                height: '45px',
+                                                fontSize: '18px',
+                                                fontWeight: 'bold',
+                                                cursor: 'pointer',
+                                            }}
                                             onMouseEnter={() => setDeleteHovered(true)}
-                                            onMouseLeave={() => setDeleteHovered(false)} 
-                                            onClick={handleDelete}>Delete</button>
-                                            <button type="button" className="btn" style={{ backgroundColor: editHovered ? '#7B556A' : '#976B7A', color: 'white', width: '125px', height: '45px', fontSize: '18px', marginLeft: '20px', marginTop:'0px', fontWeight: 'bold' }}
+                                            onMouseLeave={() => setDeleteHovered(false)}
+                                            onClick={handleDelete}
+                                        >
+                                            Delete
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="btn"
+                                            style={{
+                                                backgroundColor: editHovered ? '#7B556A' : '#976B7A',
+                                                color: 'white',
+                                                width: '125px',
+                                                height: '45px',
+                                                fontSize: '18px',
+                                                fontWeight: 'bold',
+                                                cursor: 'pointer',
+                                            }}
                                             onMouseEnter={() => setEditHovered(true)}
-                                            onMouseLeave={() => setEditHovered(false)} 
-                                            onClick={() => setOpenModal(true)}>Edit advice</button>
-                                        </div>
-                                    ) : null}
-
-                                </div>
-
+                                            onMouseLeave={() => setEditHovered(false)}
+                                            onClick={() => setOpenModal(true)}
+                                        >
+                                            Edit advice
+                                        </button>
+                                    </div>
+                                )}
                             </div>
                         </div>
-                    </>
+                    </div>
                 )}
             </div>
             {openModal && <EditAdviceModal closeModal={setOpenModal} />}
         </>
-    )
-}
+    );
+};
 
-export default SingleAdvice
+export default SingleAdvice;
